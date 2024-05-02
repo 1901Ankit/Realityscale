770943e2d8d527b357340b8a8f4d4a0dc08c3f01
Dheerendra <ankit.sharma@digixito.com>
2024-05-02 16:47:17 +0530

    addfiles

---
+++ src/assests/data/data.js
@@ -0,0 +1,80 @@
+import backgroundImage from "../images/landing/1.9f88f939.webp";
+import backgroundImage1 from "../images/gamecollection/2.082ca6f8.webp";
+// import backgroundImage2 from "../images/gamecollection/3.3c69c9c1.webp";
+import backgroundImage3 from "../images/gamecollection/4.2765c400.webp";
+// import backgroundImage4 from "../images/gamecollection/6.0a2a5eec.webp";
+import backgroundImage5 from "../images/gamecollection/5.a8fd040e.webp";
+
+let data = {
+  footerLinks: [
+    {
+      name: "ABOUT",
+      url: "/about",
+    },
+    {
+      name: "PRODUCT",
+      url: "/product",
+    },
+    {
+      name: "FEATURE",
+      url: "/feature ",
+    },
+
+    {
+      name: "CONTACT",
+      url: "/contact",
+    },
+  ],
+
+  faq: [
+    {
+      title: "	What is Lorem?",
+      message:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+      title: "		What is Lorem?",
+      message:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+      title: "		What is Lorem?",
+      message:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+      title: "	What is Lorem?",
+      message:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+    {
+      title: "		What is Lorem?",
+      message:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
+  ],
+  gameData: [
+    {
+      image: backgroundImage,
+      heading: "Thunder and City",
+    },
+    {
+      image: backgroundImage1,
+      heading: "Thunder and City",
+     
+    },
+  
+    {
+      image: backgroundImage3,
+      heading: "Thunder and City",
+     
+    },
+ 
+    {
+      image: backgroundImage5,
+      heading: "Thunder and City",
+     
+    },
+  ],
+};
+export default data;
